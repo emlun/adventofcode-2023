b39fc753396b72bf0444ccc762f55db1bdd1f42e
Emil Lundberg <emil@emlun.se>
2023-12-02 06:35:23 +0100

    Replace to_num with map in day 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,59 +1,74 @@
+use std::collections::HashMap;
+
 use crate::common::Solution;
 
-fn to_num(s: &str) -> u32 {
-    match s {
-        "0" | "zero" => 0,
-        "1" | "one" => 1,
-        "2" | "two" => 2,
-        "3" | "three" => 3,
-        "4" | "four" => 4,
-        "5" | "five" => 5,
-        "6" | "six" => 6,
-        "7" | "seven" => 7,
-        "8" | "eight" => 8,
-        "9" | "nine" => 9,
-        _ => unreachable!(),
-    }
-}
-
-fn calib(lines: &[String], patterns: &[&str]) -> u32 {
+fn calib(lines: &[String], patterns: &HashMap<&str, u32>) -> u32 {
     lines
         .iter()
         .map(|line| {
-            let first_digit = to_num(
-                patterns[patterns
-                    .iter()
-                    .enumerate()
-                    .flat_map(|(pat_i, pat)| line.find(pat).map(|i| (i, pat_i)))
-                    .min_by(|(i1, _), (i2, _)| i1.cmp(i2))
-                    .unwrap()
-                    .1],
-            );
-            let last_digit = to_num(
-                patterns[patterns
-                    .iter()
-                    .enumerate()
-                    .flat_map(|(pat_i, pat)| line.rfind(pat).map(|i| (i, pat_i)))
-                    .max_by(|(i1, _), (i2, _)| i1.cmp(i2))
-                    .unwrap()
-                    .1],
-            );
+            let first_digit = patterns[patterns
+                .keys()
+                .flat_map(|pat| line.find(pat).map(|i| (i, pat)))
+                .min_by(|(i1, _), (i2, _)| i1.cmp(i2))
+                .unwrap()
+                .1];
+            let last_digit = patterns[patterns
+                .keys()
+                .flat_map(|pat| line.rfind(pat).map(|i| (i, pat)))
+                .max_by(|(i1, _), (i2, _)| i1.cmp(i2))
+                .unwrap()
+                .1];
             10 * first_digit + last_digit
         })
         .sum()
 }
 
 fn solve_a(lines: &[String]) -> u32 {
-    let digit_pattern = &["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    calib(lines, digit_pattern)
+    let digit_pattern: HashMap<&str, u32> = [
+        ("0", 0),
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5),
+        ("6", 6),
+        ("7", 7),
+        ("8", 8),
+        ("9", 9),
+    ]
+    .iter()
+    .copied()
+    .collect();
+    calib(lines, &digit_pattern)
 }
 
 fn solve_b(lines: &[String]) -> u32 {
-    let digit_pattern = &[
-        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "zero", "one", "two", "three", "four",
-        "five", "six", "seven", "eight", "nine",
-    ];
-    calib(lines, digit_pattern)
+    let digit_pattern: HashMap<&str, u32> = [
+        ("0", 0),
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5),
+        ("6", 6),
+        ("7", 7),
+        ("8", 8),
+        ("9", 9),
+        ("zero", 0),
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]
+    .iter()
+    .copied()
+    .collect();
+    calib(lines, &digit_pattern)
 }
 
 pub fn solve(lines: &[String]) -> Solution {

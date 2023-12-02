6f1a504fc2992b23effe737d1ef510983414ce52
Emil Lundberg <emil@emlun.se>
2023-12-02 06:46:16 +0100

    Fix clippy lint

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -4,11 +4,11 @@ fn solve_a(lines: &[String]) -> u32 {
     lines
         .iter()
         .filter_map(|line| {
-            let mut splits = line.split(":");
+            let mut splits = line.split(':');
             let id: u32 = splits
                 .next()
                 .unwrap()
-                .split(" ")
+                .split(' ')
                 .nth(1)
                 .unwrap()
                 .parse()
@@ -16,11 +16,11 @@ fn solve_a(lines: &[String]) -> u32 {
             if splits
                 .next()
                 .unwrap()
-                .split(";")
+                .split(';')
                 .map(|s| s.trim())
                 .all(|sample| {
-                    sample.split(",").map(|s| s.trim()).all(|part| {
-                        let mut splits = part.split(" ");
+                    sample.split(',').map(|s| s.trim()).all(|part| {
+                        let mut splits = part.split(' ');
                         let num: u32 = splits.next().unwrap().parse().unwrap();
                         let color = splits.next().unwrap();
                         let limit = match color {
@@ -45,13 +45,13 @@ fn solve_b(lines: &[String]) -> u32 {
     lines
         .iter()
         .map(|line| {
-            let samples = line.split(":").map(|s| s.trim()).nth(1).unwrap();
+            let samples = line.split(':').map(|s| s.trim()).nth(1).unwrap();
             let mut min_red = 0;
             let mut min_green = 0;
             let mut min_blue = 0;
-            for sample in samples.split(";") {
-                for part in sample.trim().split(",") {
-                    let mut splits = part.trim().split(" ");
+            for sample in samples.split(';') {
+                for part in sample.trim().split(',') {
+                    let mut splits = part.trim().split(' ');
                     let num: u32 = splits.next().unwrap().parse().unwrap();
                     let color = splits.next().unwrap();
                     let minref = match color {

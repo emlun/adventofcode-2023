844d908dadc122dccb97581b2077952424562893
Emil Lundberg <emil@emlun.se>
2023-12-03 15:27:05 +0100

    Optimize day 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -2,19 +2,23 @@ use std::collections::HashMap;
 
 use crate::common::Solution;
 
-fn find_pattern<'p, I, V>(line: &str, patterns: &'p HashMap<&str, V>, indices: I) -> Option<&'p V>
+fn find_pattern<I>(line: &str, patterns: &HashMap<&str, u32>, indices: I) -> Option<u32>
 where
-    I: Iterator<Item = usize>,
+    I: Iterator<Item = (usize, char)>,
 {
     indices
-        .flat_map(|i| {
-            patterns.iter().find_map(|(pattern, digit)| {
-                if line[i..].starts_with(pattern) {
-                    Some(digit)
-                } else {
-                    None
-                }
-            })
+        .flat_map(|(i, c)| {
+            if c.is_numeric() {
+                Some(c.to_digit(10).unwrap())
+            } else {
+                patterns.iter().find_map(|(pattern, digit)| {
+                    if line[i..].starts_with(pattern) {
+                        Some(*digit)
+                    } else {
+                        None
+                    }
+                })
+            }
         })
         .next()
 }
@@ -23,44 +27,19 @@ fn calib(lines: &[String], patterns: &HashMap<&str, u32>) -> u32 {
     lines
         .iter()
         .map(|line| {
-            let first_digit = find_pattern(line, patterns, 0..line.len()).unwrap();
-            let last_digit = find_pattern(line, patterns, (0..line.len()).rev()).unwrap();
+            let first_digit = find_pattern(line, patterns, line.char_indices()).unwrap();
+            let last_digit = find_pattern(line, patterns, line.char_indices().rev()).unwrap();
             10 * first_digit + last_digit
         })
         .sum()
 }
 
 fn solve_a(lines: &[String]) -> u32 {
-    let digit_pattern: HashMap<&str, u32> = [
-        ("0", 0),
-        ("1", 1),
-        ("2", 2),
-        ("3", 3),
-        ("4", 4),
-        ("5", 5),
-        ("6", 6),
-        ("7", 7),
-        ("8", 8),
-        ("9", 9),
-    ]
-    .iter()
-    .copied()
-    .collect();
-    calib(lines, &digit_pattern)
+    calib(lines, &HashMap::new())
 }
 
 fn solve_b(lines: &[String]) -> u32 {
     let digit_pattern: HashMap<&str, u32> = [
-        ("0", 0),
-        ("1", 1),
-        ("2", 2),
-        ("3", 3),
-        ("4", 4),
-        ("5", 5),
-        ("6", 6),
-        ("7", 7),
-        ("8", 8),
-        ("9", 9),
         ("zero", 0),
         ("one", 1),
         ("two", 2),

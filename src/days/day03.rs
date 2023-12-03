739d404c967acffb4287ea2786531155530093a7
Emil Lundberg <emil@emlun.se>
2023-12-03 14:54:32 +0100

    Clean up and optimize day 3

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,76 +1,35 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 use crate::common::Solution;
 
-fn solve_a(lines: &[String]) -> u32 {
-    let mut parts: HashSet<(usize, usize)> = HashSet::new();
-    let mut nums: HashMap<(usize, usize, isize), u32> = HashMap::new();
-
-    for (r, line) in lines.iter().enumerate() {
-        let mut current_num = None;
-        for (c, chr) in line.chars().enumerate() {
-            if chr.is_numeric() {
-                if let Some(((r, c, l), cur)) = current_num {
-                    current_num = Some((
-                        (r, c, l + 1),
-                        cur * 10 + chr.to_string().parse::<u32>().unwrap(),
-                    ));
-                } else {
-                    current_num = Some(((r, c, 1), chr.to_string().parse().unwrap()));
-                }
-            } else {
-                if let Some((pos, num)) = current_num {
-                    nums.insert(pos, num);
-                    current_num = None;
-                }
-                if chr != '.' {
-                    parts.insert((r, c));
-                }
-            }
-        }
-        if let Some((pos, num)) = current_num {
-            nums.insert(pos, num);
-            current_num = None;
-        }
-    }
-
-    let (part_numbers, non_part_numbers): (
-        HashMap<(usize, usize, isize), u32>,
-        HashMap<(usize, usize, isize), u32>,
-    ) = nums.into_iter().partition(|((numr, numc, numl), _)| {
-        for (dr, dc) in (-1..=1).flat_map(|r| (-1..=*numl).map(move |c| (r, c))) {
-            if parts.contains(&(
-                numr.saturating_add_signed(dr),
-                numc.saturating_add_signed(dc),
-            )) {
-                return true;
-            }
-        }
-        return false;
-    });
-
-    part_numbers.into_iter().map(|(_, num)| num).sum()
+fn solve_a(numbered_parts: &HashMap<(usize, usize), (char, Vec<u32>)>) -> u32 {
+    numbered_parts.values().flat_map(|(_, nums)| nums).sum()
 }
 
-fn solve_b(lines: &[String]) -> u32 {
+fn solve_b(numbered_parts: &HashMap<(usize, usize), (char, Vec<u32>)>) -> u32 {
+    numbered_parts
+        .values()
+        .filter(|(part, nums)| *part == '*' && nums.len() == 2)
+        .map(|(_, nums)| nums.iter().product::<u32>())
+        .sum()
+}
+
+pub fn solve(lines: &[String]) -> Solution {
     let mut parts: HashMap<(usize, usize), char> = HashMap::new();
-    let mut nums: HashMap<(usize, usize, isize), u32> = HashMap::new();
+    let mut nums: Vec<(usize, usize, isize, u32)> = Vec::new();
 
     for (r, line) in lines.iter().enumerate() {
         let mut current_num = None;
         for (c, chr) in line.chars().enumerate() {
             if chr.is_numeric() {
-                if let Some(((r, c, l), cur)) = current_num {
-                    current_num = Some((
-                        (r, c, l + 1),
-                        cur * 10 + chr.to_string().parse::<u32>().unwrap(),
-                    ));
+                if let Some((r, c, l, cur)) = current_num {
+                    current_num = Some((r, c, l + 1, cur * 10 + chr.to_digit(10).unwrap()));
                 } else {
-                    current_num = Some(((r, c, 1), chr.to_string().parse().unwrap()));
+                    current_num = Some((r, c, 1, chr.to_digit(10).unwrap()));
                 }
             } else {
-                if let Some((pos, num)) = current_num {
-                    nums.insert(pos, num);
+                if let Some(num) = current_num {
+                    nums.push(num);
                     current_num = None;
                 }
                 if chr != '.' {
@@ -78,44 +37,33 @@ fn solve_b(lines: &[String]) -> u32 {
                 }
             }
         }
-        if let Some((pos, num)) = current_num {
-            nums.insert(pos, num);
-            current_num = None;
+        if let Some(num) = current_num {
+            nums.push(num);
         }
     }
 
-    parts
-        .into_iter()
-        .flat_map(|(pos, chr)| {
-            if chr == '*' {
-                let adjacent: Vec<u32> = nums
-                    .iter()
-                    .filter(|((numr, numc, numl), _)| {
-                        for (dr, dc) in (-1..=1).flat_map(|r| (-1..=*numl).map(move |c| (r, c))) {
-                            if (
-                                numr.saturating_add_signed(dr),
-                                numc.saturating_add_signed(dc),
-                            ) == pos
-                            {
-                                return true;
-                            }
-                        }
-                        return false;
-                    })
-                    .map(|(_, num)| *num)
-                    .collect();
-                if adjacent.len() == 2 {
-                    Some(adjacent.into_iter().product::<u32>())
-                } else {
-                    None
-                }
-            } else {
-                None
-            }
-        })
-        .sum()
-}
+    let mut numbered_parts: HashMap<(usize, usize), (char, Vec<u32>)> = HashMap::new();
+    for (numr, numc, numl, num) in nums {
+        for (part_pos, chr) in (-1..=1)
+            .flat_map(|dr| (-1..=numl).map(move |dc| (dr, dc)))
+            .flat_map(|(dr, dc)| {
+                let part_pos = (
+                    numr.saturating_add_signed(dr),
+                    numc.saturating_add_signed(dc),
+                );
+                parts.get(&part_pos).map(|chr| (part_pos, chr))
+            })
+        {
+            numbered_parts
+                .entry(part_pos)
+                .or_insert((*chr, Vec::new()))
+                .1
+                .push(num);
+        }
+    }
 
-pub fn solve(lines: &[String]) -> Solution {
-    (solve_a(lines).to_string(), solve_b(lines).to_string())
+    (
+        solve_a(&numbered_parts).to_string(),
+        solve_b(&numbered_parts).to_string(),
+    )
 }

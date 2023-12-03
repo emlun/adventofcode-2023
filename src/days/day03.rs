912df6040143ef40156389b88f42a466e61167fd
Emil Lundberg <emil@emlun.se>
2023-12-03 15:56:56 +0100

    Rewrite day 3 in functional style

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -15,52 +15,55 @@ fn solve_b(numbered_parts: &HashMap<(usize, usize), (char, Vec<u32>)>) -> u32 {
 }
 
 pub fn solve(lines: &[String]) -> Solution {
-    let mut parts: HashMap<(usize, usize), char> = HashMap::new();
-    let mut nums: Vec<(usize, usize, isize, u32)> = Vec::new();
+    type NumberLocation = (usize, usize, isize, u32);
+    let (parts, nums): (HashMap<(usize, usize), char>, Vec<NumberLocation>) = lines
+        .iter()
+        .enumerate()
+        .fold((HashMap::new(), Vec::new()), |(parts, nums), (r, line)| {
+            let (parts, mut nums, unfinished_num) = line.chars().enumerate().fold(
+                (parts, nums, None),
+                |(mut parts, mut nums, mut next_num), (c, chr)| {
+                    if let Some(digit) = chr.to_digit(10) {
+                        let (_, _, l, cur) = next_num.get_or_insert((r, c, 0, 0));
+                        *l += 1;
+                        *cur = *cur * 10 + digit;
+                    } else {
+                        nums.extend(next_num.take());
+                        if chr != '.' {
+                            parts.insert((r, c), chr);
+                        }
+                    }
+                    (parts, nums, next_num)
+                },
+            );
+            nums.extend(unfinished_num);
 
-    for (r, line) in lines.iter().enumerate() {
-        let mut current_num = None;
-        for (c, chr) in line.chars().enumerate() {
-            if chr.is_numeric() {
-                if let Some((r, c, l, cur)) = current_num {
-                    current_num = Some((r, c, l + 1, cur * 10 + chr.to_digit(10).unwrap()));
-                } else {
-                    current_num = Some((r, c, 1, chr.to_digit(10).unwrap()));
-                }
-            } else {
-                if let Some(num) = current_num {
-                    nums.push(num);
-                    current_num = None;
-                }
-                if chr != '.' {
-                    parts.insert((r, c), chr);
-                }
+            (parts, nums)
+        });
+
+    let numbered_parts: HashMap<(usize, usize), (char, Vec<u32>)> = nums.into_iter().fold(
+        HashMap::new(),
+        |mut numbered_parts, (numr, numc, numl, num)| {
+            for (part_pos, chr) in (-1..=1)
+                .flat_map(|dr| (-1..=numl).map(move |dc| (dr, dc)))
+                .filter(|drc| *drc != (0, 0))
+                .flat_map(|(dr, dc)| {
+                    let part_pos = (
+                        numr.saturating_add_signed(dr),
+                        numc.saturating_add_signed(dc),
+                    );
+                    parts.get(&part_pos).map(|chr| (part_pos, chr))
+                })
+            {
+                numbered_parts
+                    .entry(part_pos)
+                    .or_insert((*chr, Vec::new()))
+                    .1
+                    .push(num);
             }
-        }
-        if let Some(num) = current_num {
-            nums.push(num);
-        }
-    }
-
-    let mut numbered_parts: HashMap<(usize, usize), (char, Vec<u32>)> = HashMap::new();
-    for (numr, numc, numl, num) in nums {
-        for (part_pos, chr) in (-1..=1)
-            .flat_map(|dr| (-1..=numl).map(move |dc| (dr, dc)))
-            .flat_map(|(dr, dc)| {
-                let part_pos = (
-                    numr.saturating_add_signed(dr),
-                    numc.saturating_add_signed(dc),
-                );
-                parts.get(&part_pos).map(|chr| (part_pos, chr))
-            })
-        {
             numbered_parts
-                .entry(part_pos)
-                .or_insert((*chr, Vec::new()))
-                .1
-                .push(num);
-        }
-    }
+        },
+    );
 
     (
         solve_a(&numbered_parts).to_string(),

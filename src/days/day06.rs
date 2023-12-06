4c0475eddccd5c3d996a331fe8842a2377430054
Emil Lundberg <emil@emlun.se>
2023-12-06 07:22:12 +0100

    Optimize day 6

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,15 +1,27 @@
 use crate::common::Solution;
 
+/// Solve the equation x^2 + px + q = 0, assuming real solutions exist.
+fn solve_quadratic(p: f64, q: f64) -> (f64, f64) {
+    let p_half = p / 2.0;
+    let sqrt_part = ((p / 2.0).powi(2) - q).sqrt();
+    (-p_half - sqrt_part, -p_half + sqrt_part)
+}
+
+#[cfg(test)]
+#[test]
+fn test_solve_quadratic() {
+    assert_eq!((0.0, 2.0), solve_quadratic(-2.0, 0.0));
+    let (x1, x2) = solve_quadratic(0.0, 1.0);
+    assert!(x1.is_nan());
+    assert!(x2.is_nan());
+}
+
 fn solve_a(races: &[(u64, u64)]) -> usize {
     races
         .iter()
         .map(|(time, distance)| {
-            (0..=*time)
-                .filter(|t| {
-                    let d = (time - t) * t;
-                    d > *distance
-                })
-                .count()
+            let (t1, t2) = solve_quadratic(-(*time as f64), *distance as f64);
+            (t2.floor() - t1.ceil()) as usize + 1
         })
         .product()
 }
@@ -23,13 +35,8 @@ fn solve_b(races: &[(u64, u64)]) -> usize {
                 distance * u64::from(10_u32.pow(d.ilog10() + 1)) + *d,
             )
         });
-
-    (0..=time)
-        .filter(|t| {
-            let d = (time - t) * t;
-            d > distance
-        })
-        .count()
+    let (t1, t2) = solve_quadratic(-(time as f64), distance as f64);
+    (t2.floor() - t1.ceil()) as usize + 1
 }
 
 pub fn solve(lines: &[String]) -> Solution {
@@ -43,8 +50,6 @@ pub fn solve(lines: &[String]) -> Solution {
         .skip(1)
         .flat_map(|s| s.parse().ok())
         .collect();
-
     let races: Vec<(u64, u64)> = times.into_iter().zip(distances.into_iter()).collect();
-
     (solve_a(&races).to_string(), solve_b(&races).to_string())
 }

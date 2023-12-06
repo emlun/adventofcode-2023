dfa39f9437928fadebf3172ae7dad15e39a270ed
Emil Lundberg <emil@emlun.se>
2023-12-06 06:17:39 +0100

    Solve day 6b

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -14,6 +14,24 @@ fn solve_a(races: &[(u64, u64)]) -> usize {
         .product()
 }
 
+fn solve_b(races: &[(u64, u64)]) -> usize {
+    let (time, distance) = races
+        .iter()
+        .fold((0_u64, 0_u64), |(time, distance), (t, d)| {
+            (
+                time * u64::from(10_u32.pow(t.ilog10() + 1)) + *t,
+                distance * u64::from(10_u32.pow(d.ilog10() + 1)) + *d,
+            )
+        });
+
+    (0..=time)
+        .filter(|t| {
+            let d = (time - t) * t;
+            d > distance
+        })
+        .count()
+}
+
 pub fn solve(lines: &[String]) -> Solution {
     let times: Vec<u64> = lines[0]
         .split_whitespace()
@@ -28,5 +46,5 @@ pub fn solve(lines: &[String]) -> Solution {
 
     let races: Vec<(u64, u64)> = times.into_iter().zip(distances.into_iter()).collect();
 
-    (solve_a(&races).to_string(), "".to_string())
+    (solve_a(&races).to_string(), solve_b(&races).to_string())
 }

4a5db3502e6be7f555a0f1cf05a3e199db368ddd
Emil Lundberg <emil@emlun.se>
2023-12-09 07:27:49 +0100

    Sovle day 9b

--- src/days/day09.rs
+++ src/days/day09.rs
@@ -1,17 +1,27 @@
 use crate::{common::Solution, util::iter::WithSliding};
 
-fn predict(values: &[i64]) -> i64 {
+fn predict(values: &[i64], forward: bool) -> i64 {
     let diffs: Vec<i64> = values.iter().sliding2().map(|(a, b)| b - a).collect();
-    values.last().unwrap()
-        + (if diffs.iter().all(|a| *a == 0) {
-            0
-        } else {
-            predict(&diffs)
-        })
+
+    let predicted_diff = if diffs.iter().all(|a| *a == 0) {
+        0
+    } else {
+        predict(&diffs, forward)
+    };
+
+    if forward {
+        values.last().unwrap() + predicted_diff
+    } else {
+        values.first().unwrap() - predicted_diff
+    }
 }
 
 fn solve_a(series: &[Vec<i64>]) -> i64 {
-    series.iter().map(|nums| predict(nums)).sum()
+    series.iter().map(|nums| predict(nums, true)).sum()
+}
+
+fn solve_b(series: &[Vec<i64>]) -> i64 {
+    series.iter().map(|nums| predict(nums, false)).sum()
 }
 
 pub fn solve(lines: &[String]) -> Solution {
@@ -26,5 +36,5 @@ pub fn solve(lines: &[String]) -> Solution {
         })
         .collect();
 
-    (solve_a(&series).to_string(), "".to_string())
+    (solve_a(&series).to_string(), solve_b(&series).to_string())
 }

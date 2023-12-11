a99c3a93ef6440bd9669d1d4b2fc96844eaa5c31
Emil Lundberg <emil@emlun.se>
2023-12-11 06:34:24 +0100

    Solve day 11b

--- src/days/day11.rs
+++ src/days/day11.rs
@@ -3,7 +3,53 @@ use std::collections::HashSet;
 
 use crate::common::Solution;
 
-fn solve_a(map: &HashSet<(usize, usize)>) -> usize {
+fn expand(map: &[Vec<bool>], size: usize) -> HashSet<(usize, usize)> {
+    let size = size - 1;
+    let expand_rows: Vec<usize> = map
+        .iter()
+        .enumerate()
+        .filter(|(_, row)| row.iter().all(|b| !*b))
+        .map(|(r, _)| r)
+        .collect();
+    let expand_cols: Vec<usize> = (0..map[0].len())
+        .filter(|c| map.iter().all(|row| !row[*c]))
+        .collect();
+
+    map.iter()
+        .enumerate()
+        .flat_map(|(r, row)| {
+            row.into_iter()
+                .enumerate()
+                .filter(|(_, b)| **b)
+                .map(move |(c, _)| (c, r))
+        })
+        .map(|(c, r)| {
+            let col_expansion_factor = expand_cols.iter().take_while(|cc| **cc < c).count();
+            let row_expansion_factor = expand_rows.iter().take_while(|rr| **rr < r).count();
+            (
+                c + col_expansion_factor * size,
+                r + row_expansion_factor * size,
+            )
+        })
+        .collect()
+}
+
+fn solve_a(map: &[Vec<bool>]) -> usize {
+    let map = expand(map, 2);
+    map.iter()
+        .flat_map(|pos1| {
+            map.iter().filter(move |pos2| pos1 != *pos2).map(|pos2| {
+                let (x1, y1) = pos1;
+                let (x2, y2) = pos2;
+                x1.abs_diff(*x2) + y1.abs_diff(*y2)
+            })
+        })
+        .sum::<usize>()
+        / 2
+}
+
+fn solve_b(map: &[Vec<bool>]) -> usize {
+    let map = expand(map, 1000_000);
     map.iter()
         .flat_map(|pos1| {
             map.iter().filter(move |pos2| pos1 != *pos2).map(|pos2| {
@@ -30,35 +76,5 @@ pub fn solve(lines: &[String]) -> Solution {
         })
         .collect();
 
-    let map = map.into_iter().fold(Vec::new(), |mut result, row| {
-        if row.iter().all(|b| !*b) {
-            result.push(row.clone());
-        }
-        result.push(row);
-        result
-    });
-
-    let (map, _): (Vec<Vec<bool>>, _) =
-        (0..map[0].len()).fold((map, 0), |(mut result, mut dc), c| {
-            if result.iter().all(|row| !row[c + dc]) {
-                for row in result.iter_mut() {
-                    row.insert(c + dc, false);
-                }
-                dc += 1;
-            }
-            (result, dc)
-        });
-
-    let map: HashSet<(usize, usize)> = map
-        .into_iter()
-        .enumerate()
-        .flat_map(|(r, row)| {
-            row.into_iter()
-                .enumerate()
-                .filter(|(_, b)| *b)
-                .map(move |(c, _)| (c, r))
-        })
-        .collect();
-
-    (solve_a(&map).to_string(), "".to_string())
+    (solve_a(&map).to_string(), solve_b(&map).to_string())
 }

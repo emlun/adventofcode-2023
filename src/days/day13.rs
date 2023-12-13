ab97c1e4d27e170cfca069d7926acacf1e8ef27f
Emil Lundberg <emil@emlun.se>
2023-12-13 06:44:05 +0100

    Solve day 13b

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -32,6 +32,45 @@ fn solve_a(patterns: &[Vec<Vec<bool>>]) -> usize {
         .sum()
 }
 
+fn solve_b(patterns: &[Vec<Vec<bool>>]) -> usize {
+    patterns
+        .iter()
+        .map(|pattern| {
+            let horiz = (1..(pattern[0].len()))
+                .find(|reflection_col| {
+                    pattern
+                        .iter()
+                        .map(|row| {
+                            std::iter::zip(
+                                row.iter().take(*reflection_col).rev(),
+                                row.iter().skip(*reflection_col),
+                            )
+                            .filter(|(a, b)| a != b)
+                            .count()
+                        })
+                        .sum::<usize>()
+                        == 1
+                })
+                .unwrap_or(0);
+
+            let vert = (1..(pattern.len()))
+                .find(|reflection_row| {
+                    std::iter::zip(
+                        pattern.iter().take(*reflection_row).rev(),
+                        pattern.iter().skip(*reflection_row),
+                    )
+                    .map(|(row_a, row_b)| row_a.iter().zip(row_b).filter(|(a, b)| a != b).count())
+                    .sum::<usize>()
+                        == 1
+                })
+                .unwrap_or(0);
+
+            assert!(vert == 0 || horiz == 0);
+            100 * vert + horiz
+        })
+        .sum()
+}
+
 pub fn solve(lines: &[String]) -> Solution {
     let (mut patterns, last_pattern): (Vec<Vec<Vec<bool>>>, _) =
         lines.iter().map(|s| s.trim()).fold(
@@ -52,5 +91,8 @@ pub fn solve(lines: &[String]) -> Solution {
         patterns.push(last_pattern);
     }
 
-    (solve_a(&patterns).to_string(), "".to_string())
+    (
+        solve_a(&patterns).to_string(),
+        solve_b(&patterns).to_string(),
+    )
 }

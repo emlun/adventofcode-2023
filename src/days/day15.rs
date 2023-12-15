98b662a512e683cb9ee086bdff57fcebc1d9ba36
Emil Lundberg <emil@emlun.se>
2023-12-15 11:23:19 +0100

    Solve day 15b

--- src/days/day15.rs
+++ src/days/day15.rs
@@ -10,6 +10,46 @@ fn solve_a(steps: &[&str]) -> u32 {
     steps.iter().map(|step| u32::from(hash(step))).sum()
 }
 
+fn solve_b(steps: &[&str]) -> usize {
+    steps
+        .iter()
+        .fold(
+            std::array::from_fn(Vec::with_capacity),
+            |mut map: [Vec<(&str, u8)>; 256], step| {
+                let (label, flen, add): (&str, u8, bool) =
+                    if let Some((label, flen)) = step.split_once('=') {
+                        (label, flen.parse().unwrap(), true)
+                    } else if let Some((label, _)) = step.split_once('-') {
+                        (label, 0, false)
+                    } else {
+                        unimplemented!()
+                    };
+                let hash = hash(label) as usize;
+                if add {
+                    if let Some((_, existing_flen)) =
+                        map[hash].iter_mut().find(|(l, _)| *l == label)
+                    {
+                        *existing_flen = flen;
+                    } else {
+                        map[hash].push((label, flen));
+                    }
+                } else {
+                    map[hash].retain(|(l, _)| *l != label);
+                }
+                map
+            },
+        )
+        .iter()
+        .enumerate()
+        .flat_map(|(i_box, lenses)| {
+            lenses
+                .iter()
+                .enumerate()
+                .map(move |(i_lens, (_, flen))| (i_box + 1) * (i_lens + 1) * (*flen as usize))
+        })
+        .sum()
+}
+
 pub fn solve(lines: &[String]) -> Solution {
     let steps: Vec<&str> = lines
         .iter()
@@ -18,5 +58,5 @@ pub fn solve(lines: &[String]) -> Solution {
         .flat_map(|line| line.split(','))
         .collect();
 
-    (solve_a(&steps).to_string(), "".to_string())
+    (solve_a(&steps).to_string(), solve_b(&steps).to_string())
 }
